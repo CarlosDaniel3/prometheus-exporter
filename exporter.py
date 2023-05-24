68759a531f11b401113b196691f6d5d0be76b4b9
Carlos Daniel <carloscggames@gmail.com>
2023-05-23 22:52:55 -0300

    create exporter.py

---
+++ exporter.py
@@ -0,0 +1,47 @@
+import requests
+import json
+import time
+from prometheus_client import start_http_server, Gauge
+
+url_numero_pessoas = "http://api.open-notify.org/astros.json"
+
+def pega_numero_astronautas():
+    try:
+        response = requests.get(url_numero_pessoas)
+        data = response.json()
+        return data['number']
+    except Exception as e:
+        print("Tivemos problemas para acessar a URL")
+        raise e
+
+def atualiza_metricas():
+    try:
+        numero_pessoas = Gauge('numero_de_astronautas', 'Número de astronautas no espaço')
+        while True:
+            numero_pessoas.set(pega_numero_astronautas())
+            time.sleep(10)
+            print("O número de Astronautas no espaço nesse momento é: %s" %pega_numero_astronautas())
+    except Exception as e:
+        print("Tivemos problemas em atualizar a métrica")
+        raise e
+
+def inicia_exporter():
+    try:
+        start_http_server(8899)
+        return True
+    except Exception as e:
+        print("Tivemos problemas em iniciar o http server")
+        raise e
+
+def main():
+    try:
+        inicia_exporter()
+        print("HTTP Server iniciado")
+        atualiza_metricas()
+    except Exception as e:
+        print("Tivemos problemas na inicialização do nosso exporter")
+        exit(1)
+
+if __name__ == '__main__':
+    main()
+    exit(0)
